e7b013e1eed99bae66dfc448a78d775072b3216b
ChangwooJ <wjdckddn99@naver.com>
2025-06-25 19:21:34 +0900

    refactor: 헤더 디자인 반영

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import backIcon from "../assets/backIcon.svg";
 
 export default function Layout() {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(-1);
+  }
+
   return (
     <div className="h-screen flex flex-col">
-      <header className="bg-[#D9D9D9] h-[116px] flex justify-center items-center">
-        <p className="text-[18px] font-extrabold">얼레벌레열심히</p>
+      <header className="bg-[#F1F5F4] h-[129px] flex justify-center items-end border-b-[3.5px] border-[#D9E3E0]">
+        <div className="w-full h-50 flex justify-between">
+          <span className="ml-30 mt-5" onClick={handleNavigate}><img src={backIcon} alt="뒤로가기" width="11" height="25" /></span>
+          <p className="text-[18px] font-extrabold">톡스터디</p>
+          <span className="w-[41px]"></span>
+        </div>
       </header>
       <main className="relative flex-1 min-h-0 overflow-y-auto">
         <Outlet />
