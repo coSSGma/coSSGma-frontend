900a3c0e533c4dfd6ce0a45301f09e16160633c0
ChangwooJ <wjdckddn99@naver.com>
2025-06-25 14:54:35 +0900

    feat: Layout 프레임 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './feature/login/login';
+import Layout from './layout/Layout';
 
 function App() {  
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<Layout />}>
+        
+        </Route>
+        <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,34 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div className="h-screen flex flex-col">
+      <header className="bg-[#D9D9D9] h-[116px]">Header</header>
+      <main className="relative flex-1 min-h-0 overflow-y-auto">
+        <Outlet />
+      </main>
+      <footer className="bg-[#D9D9D9] py-20 h-[92px] flex justify-evenly">
+        <button
+          className="w-[52px] h-[52px] rounded-full bg-white text-[15px] font-bold"
+        >
+          홈
+        </button>
+        <button
+          className="w-[52px] h-[52px] rounded-full bg-white text-[15px] font-bold"
+        >
+          스터디
+        </button>
+        <button
+          className="w-[52px] h-[52px] rounded-full bg-white text-[15px] font-bold"
+        >
+          랭킹
+        </button>
+        <button
+          className="w-[52px] h-[52px] rounded-full bg-white text-[15px] font-bold"
+        >
+          마이<br />페이지
+        </button>
+      </footer>
+    </div>
+  );
+}
